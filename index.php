57ff392fb74d2b61a912ec388fc29d8ec6893c68
Huig <153827936+Huig01@users.noreply.github.com>
2024-03-18 13:29:07 +0100

    yes
    yes

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include "pages/conn.php"; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- pages/conn.php
+++ pages/conn.php
@@ -7,15 +7,15 @@ $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXPECTION,
-    PDO::ATTR_DEFAULT_FETCH => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 
 ];
 try{
     $connection = new PDO($dsn, $user, $pass, $options);
     echo "Connected to database";
 }catch (\PDOExpection $e){
-    echo "Connection failed:" . $e=>getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
