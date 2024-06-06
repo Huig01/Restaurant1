1ed5d47de01ad3221bba60303644b2a0de653b72
Huig <153827936+Huig01@users.noreply.github.com>
2024-06-06 11:08:23 +0200

    yes
    yes

--- pages/product_delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include 'conn.php';
- 
-$stmt = $connection->prepare("DELETE FROM products WHERE id=:id");
-$stmt->execute(['id' => $_GET['id']]);
-$product = $stmt->fetch();
-header('Location: ../index.php');
-?>
