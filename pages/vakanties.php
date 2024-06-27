6b3bb1ccad8a1b8f6fd62097f7f87bbbf5b38605
Huig <153827936+Huig01@users.noreply.github.com>
2024-06-27 11:17:09 +0200

    yes
    yes

--- pages/vakanties.php
+++ pages/vakanties.php
@@ -27,9 +27,9 @@
     </div>
 </div>
 <div class="vakantie_block">
-    <h2>
+    <h4>
         Vakanties
-    </h2>
+    </h4>
     <hr class = "hr1">
     <div class="vakantie_block2">        
         <div class="vakantie_block_links">
@@ -61,55 +61,156 @@
         </div>
         <div class="checkbox-vakantie-block">
             <div class="landencheckbox">
-                <div class="landen-block">                   
-                    <div class="plaatsen">
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>                  
+                <div class="h2">
+                    <h2>LUCHTHAVEN</h2>
+                </div>
+                <div class="plaatsen">
+                    <div class="check_icon3">
+                        <i class="fa-regular fa-square"></i>
                     </div>
-                    <div class="plaatsen-nederland">
-                    <a>Amsterdam</a>
-                    <br>
-                    <a>Eindhoven</a>
-                    <a>Groningen</a>
-                    <a>Maastricht</a>
-                    <a>Rotterdam</a>
+                    <div class="hotel_expertiese2">
+                        <a>Nederland</a>
                     </div>
-                    <div class="plaatsen">
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-regular fa-square"></i>                  
+                </div>
+                <div class="opties-plaatsen">
+                    <div class="check_icon2">
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
                     </div>
-                    <div class="plaatsen-nederland">
-                    <a>Amsterdam</a>
-                    <br>
-                    <a>Eindhoven</a>
-                    <a>Groningen</a>
-                    <a>Maastricht</a>
-                    <a>Rotterdam</a>
-                    </div>                          
-                </div>            
-            </div>        
+                    <div class="hotel_expertiese">
+                        <a>Amsterdam</a>
+                        <a>Eindhoven</a>
+                        <a>Groningen</a>
+                        <a>Maastricht</a>
+                        <a>Rotterdam</a>
+                    </div>                   
+                </div>
+                <div class="plaatsen">
+                    <div class="check_icon3">
+                        <i class="fa-regular fa-square"></i>
+                    </div>
+                    <div class="hotel_expertiese2">
+                        <a>België</a>
+                    </div>
+                </div>
+                <div class="opties-plaatsen">
+                    <div class="check_icon2">
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                    </div>
+                    <div class="hotel_expertiese">
+                        <a>Antwerpen</a>
+                        <a>Brussel Charleroi</a>
+                        <a>Brussel Zaventem</a>
+                        <a>Luik</a>
+                        <a>Oostende</a>
+                    </div>                  
+                </div>
+                <div class="plaatsen">
+                    <div class="check_icon3">
+                        <i class="fa-regular fa-square"></i>
+                    </div>
+                    <div class="hotel_expertiese2">
+                        <a>Duitsland</a>
+                    </div>
+                </div>
+                <div class="opties-plaatsen">
+                    <div class="check_icon2">
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>                        
+                    </div>
+                    <div class="hotel_expertiese">
+                        <a>Dusseldorf</a>
+                        <a>Keulen</a>
+                        <a>Munster</a>
+                        <a>Weeze</a>                       
+                    </div>                   
+                </div>
+                <div class="h3">
+                    <h3>VERBLIJF</h3>
+                </div>
+                <div class="h2">
+                    <h2>VERZORGING</h2>
+                </div>
+                <div class="opties-plaatsen">
+                    <div class="check_icon2">
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>                         
+                    </div>
+                    <div class="hotel_expertiese">
+                        <a>All Inclusive</a>
+                        <a>Halfpension</a>
+                        <a>Logies</a>
+                        <a>Logies En Ontbijt</a> 
+                        <a>Ultra All Inclusive</a>                        
+                    </div>                   
+                </div>
+                <div class="h2">
+                    <h2>ACCOMMODATIETYPE</h2>
+                </div>
+                <div class="opties-plaatsen">
+                    <div class="check_icon2">
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>
+                        <i class="fa-regular fa-square"></i>                         
+                    </div>
+                    <div class="hotel_expertiese">
+                        <a>Appartement</a>
+                        <a>Hotel</a>
+                        <a>Camping</a>
+                        <a>Vakantiepark</a>
+                        <a>Woning</a>                        
+                    </div>                   
+                </div>                                                     
+            </div>             
         </div>
         </div>
-        <div class="vakantie_block_recht">
-            <div class="vakantie-row">
+        <div class="vakantie_block_recht">            
+            <div class="vakantie_row">
             <div class="foto_vakantie">
                 <div class="ft_vakantie1"></div>                         
             </div>
             <div class="hotel_name_block">
-                <div class="hotel_name">
-                    <h3>
-                    Hotel Allsun Marena Beach
-                    </h3>
-                    <a>Playa de Palma, Mallorca, Spanje</a>
+                <div class="hotel_name1">
+                    <div class="h3">
+                        <h3>
+                            Hotel Allsun Marena Beach                                                   
+                        </h3>                                                                 
+                    </div>
+                    <div class="stars">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>                           
+                </div>
+                <div class="place">
+                    <div class="flag-country1"></div>
+                    <div class="place-name">
+                    Playa de Palma, Mallorca, Spanje
+                    </div>
                 </div>
                 <div class="rating_block">
                     <div class="rating">
-
+                        <a>8.7</a>
                     </div>
+                    <div class="recenties">
+                    <a>GEWELDIG</a>
+                    <a>200+beoordelen</a>
+                    </div>                    
                 </div>
                 <div class="hotel-name2">
                     <div class="check_icon">
@@ -126,7 +227,82 @@
                     <a>Dicht bij het strand gelegen</a>
                     </div>                    
                 </div>                                
-            </div>                                   
+            </div>              
+            </div>
+            <div class="hr3">
+            <hr class = "hr">
+            </div>
+            <div class="vakantie_row2">
+            <div class="foto_vakantie">
+                <div class="ft_vakantie2"></div>                         
+            </div>
+            <div class="hotel_name_block">
+            <div class="hotel_name1">
+                    <div class="h3">
+                        <h3>
+                        Hotel MGallery Le Louis Versailles Chateau                                                    
+                        </h3>                                                                 
+                    </div>
+                    <div class="stars">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>                           
+                </div>
+                <div class="place">
+                    <div class="flag-country2"></div>
+                    <div class="place-name">
+                    Versailles - Ile De France - Frankrijk
+                    </div>
+                </div>
+                <div class="rating_block">
+                    <div class="rating">
+                        <a>8.3</a>
+                    </div>
+                    <div class="recenties">
+                    <a>ZEER GOED</a>
+                    <a>500+ beoordelen</a>
+                    </div>                    
+                </div>
+                <div class="hotel-name2">
+                    <div class="check_icon">
+                    <i class="fa-solid fa-check"></i>
+                    <i class="fa-solid fa-check"></i>
+                    <i class="fa-solid fa-check"></i>
+                    <i class="fa-solid fa-check"></i>
+                    </div>
+                    <div class="hotel_expertiese">                    
+                    <a>Zwembad met mooi zeezicht</a>
+                    <a>Diverse sport- en spafaciliteiten
+                    </a>
+                    <a>Culinair genieten op het terras</a>
+                    <a>Dicht bij het strand gelegen</a>
+                    </div>                    
+                </div>                                
+            </div>                                                              
         </div>
+        <div class="hr3">
+            <hr class = "hr3">
+            </div>
+        <div class="next_page">
+            <div class="next_page_2">
+                <div class="page_1">
+                    <a>1</a>
+                </div>
+                <div class="page_2">
+                    <a>2</a>
+                </div>
+                <div class="page_3">
+                    <a>3</a>                    
+                </div>
+                <div class="page_4">
+                    <a>4</a>                    
+                </div>
+                <div class="arrow-right">
+                <i class="fa-solid fa-arrow-right"></i>
+                </div>                
+            </div>
+        </div>  
     </div>    
 </div>
